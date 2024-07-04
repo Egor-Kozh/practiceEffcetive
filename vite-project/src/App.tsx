6a62485ed3074318122f06b85cde7a2cbf5bb81b
Egor-Kozh <kozhevnikovegor55@gmail.com>
2024-07-04 08:12:07 +0600

    list_characters

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
 import './Header.css'
 import './Footer.css'
+import Characters from './Characters'
 import marvel_logo from '/marvel-logo.png'
 import marvel_logo_gr from '/marvel-grey.png'
-import spider_man from '/spider_man.jpg'
 
 function App() {
   return (
@@ -43,20 +43,7 @@ function Body(){
             <button className='search_button'>SEARCH</button>
             </div>
           </div>
-          <div className='characters'>
-            <Character />
-            <Character />
-            <Character />
-            <Character />
-            <Character />
-            <Character />
-            <Character />
-            <Character />
-            <Character />
-            <Character />
-            <Character />
-            <Character />
-          </div>
+          <Characters />
         </div>
       </div>
     </>
@@ -73,25 +60,6 @@ function Footer() {
         <h2>Data provided by Marvel. © 2024 MARVEL</h2>
         <a href="https://developer.marvel.com/">developer.marvel.com</a>
       </header>
-    </>
-  )
-}
-
-function Character() {
-  return (
-    <>
-      <div className='character'>
-        <div className='logo_character'>
-        <img src={ spider_man } alt="spider" className='spider-man'/>
-        </div>
-        <div className='info'>
-          <h3>Spider Man</h3>
-          <div className='description'>
-            A superhero appearing in Marvel Comics, created by Stan Lee and Steve Ditko.
-             Since his first appearance on the pages of Amazing Fantasy comics.
-          </div>
-        </div>
-      </div>
     </>
   )
 }
---
+++ vite-project/src/Characters.tsx
@@ -0,0 +1,79 @@
+import React from "react"
+import './App.css'
+
+class Characters extends React.Component {
+    characters = [
+        {
+            id: 1,
+            name: "Spider Man",
+            description: "A superhero appearing in Marvel Comics, created by Stan Lee and Steve Ditko.Since his first appearance on the pages of Amazing Fantasy comics.",
+            logo: "spider_man.jpg",
+        },
+        {
+            id: 2,
+            name: "Iron Man",
+            description: "A superhero appearing in Marvel Comics, created by Stan Lee and Steve Ditko.Since his first appearance on the pages of Amazing Fantasy comics.",
+            logo: "iron_man.jpg",
+        },
+        {
+            id: 3,
+            name: "Dr. Strange",
+            description: "A superhero appearing in Marvel Comics, created by Stan Lee and Steve Ditko.Since his first appearance on the pages of Amazing Fantasy comics.",
+            logo: "dr_strange.jpg",
+        },
+        {
+            id: 4,
+            name: "Tor",
+            description: "A superhero appearing in Marvel Comics, created by Stan Lee and Steve Ditko.Since his first appearance on the pages of Amazing Fantasy comics.",
+            logo: "tor.jpg",
+        },
+        {
+            id: 5,
+            name: "Hulk",
+            description: "A superhero appearing in Marvel Comics, created by Stan Lee and Steve Ditko.Since his first appearance on the pages of Amazing Fantasy comics.",
+            logo: "hulk.jpg",
+        },
+        {
+            id: 6,
+            name: "Captain America",
+            description: "A superhero appearing in Marvel Comics, created by Stan Lee and Steve Ditko.Since his first appearance on the pages of Amazing Fantasy comics.",
+            logo: "сaptain_america.jpg",
+        },
+        {
+            id: 7,
+            name: "Black Panther",
+            description: "A superhero appearing in Marvel Comics, created by Stan Lee and Steve Ditko.Since his first appearance on the pages of Amazing Fantasy comics.",
+            logo: "black_panther.jpg",
+        },
+        {
+            id: 8,
+            name: "Wolverine",
+            description: "A superhero appearing in Marvel Comics, created by Stan Lee and Steve Ditko.Since his first appearance on the pages of Amazing Fantasy comics.",
+            logo: "wolverine.jpg",
+        }
+    ]
+
+    render() {
+        return (
+            <div className="characters">
+                {this.characters.map((el) => (
+                    <div className='character'>
+                        <div className='logo_character'>
+                            <img src={el.logo} alt="logo" className='logo'/>
+                        </div>
+                        <div className='info'>
+                            <h3>{el.name}</h3>
+                            <div className='description'>
+                                {el.description}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+        )
+    }
+        
+}
+
+export default Characters
